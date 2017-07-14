f5334436d5ba862d4479c809bc28bfd1630d4b58
ramireddygangadhar <ramireddygangadhar@gmail.com>
2017-07-14 16:07:05 +0530

    Update SampleController.java

--- src/com/bt/contoller/SampleController.java
+++ src/com/bt/contoller/SampleController.java
@@ -20,7 +20,7 @@ public class SampleController {
 			throws JSONException {
 
 		JSONObject js = new JSONObject();
-		js.put("Name", "Bhaskar Reddy Lacchannagari");
+		js.put("Name", "Gangadhar Reddy");
 		js.put("Technology", "DevOps ");
 
 		return js.toString();
