9539dfbcd281d160f915a45532c482ee6616a80e
vsdileep01 <vsdileep01@gmail.com>
2017-07-14 17:02:32 +0530

    Update SampleController.java

--- src/com/bt/contoller/SampleController.java
+++ src/com/bt/contoller/SampleController.java
@@ -20,7 +20,7 @@ public class SampleController {
 			throws JSONException {
 
 		JSONObject js = new JSONObject();
-		js.put("Name", "ajaydilip ");
+		js.put("Name", "ajay");
 		js.put("Technology", "DevOps ");
 
 		return js.toString();
